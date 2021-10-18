ba81d1b55f9dfb8e804fe80c0de4f925fe5c3731
Rafael S. Guimarães <rafaelsg@gmail.com>
2021-10-18 12:58:47 -0300

    add aula03 with udp server and client as example

---
+++ aula03/client_udp.py
@@ -0,0 +1,28 @@
+import socket
+import sys
+
+HOST = "127.0.0.1"  # Endereco IP do S
+PORT = 5000  # Porta que o Servidor esta
+
+
+def client():
+    udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    orig = ("", 5000)
+    dest = (HOST, PORT)
+    udp.bind(orig)
+    print("Type q to exit")
+    message = None
+    while message != "q":
+        if sys.version_info.major == 3:
+            message = input("-> ")
+        else:
+            message = raw_input("-> ")
+        udp.sendto(message.encode('utf-8'), dest)
+        # data = udp.recv(1024).decode("utf-8")
+        # print("Received from server: " + data)
+    udp.close()
+
+
+if __name__ == "__main__":
+    client()
+
