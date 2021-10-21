3e27aea3b098e963c1c312d23f8d729450a49517
Rafael S. Guimarães <rafaelsg@gmail.com>
2021-10-21 15:47:51 -0300

    add example for tarefa 02

--- aula03/client_server_udp.py
+++ aula03/client_server_udp.py
@@ -1,15 +1,28 @@
 import socket
 import sys
+import _thread
 
-HOST = "127.0.0.1"  # Endereco IP do S
+HOST = sys.argv[1]  # Endereco IP do S
 PORT = 5000  # Porta que o Servidor esta
+
+
+def server(udp):
+    print(f"Starting UDP Server on port {PORT}")
+    udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    orig = (HOST, PORT)
+    udp.bind(orig)
+    while True:
+        msg, cliente = udp.recvfrom(1024)
+        msg_decoded = msg.decode('utf-8')
+        print(f"{cliente} {msg_decoded}")
 
 
 def client():
     udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    orig = ("", 5000)
+    orig = ("", PORT)
     dest = (HOST, PORT)
     udp.bind(orig)
+    _thread.start_new_thread(server, (udp,))
     print("Type q to exit")
     message = None
     while message != "q":
@@ -17,7 +30,7 @@ def client():
             message = input("-> ")
         else:
             message = raw_input("-> ")
-        udp.sendto(message.encode('utf-8'), dest)
+        udp.sendto(message.encode("utf-8"), dest)
         # data = udp.recv(1024).decode("utf-8")
         # print("Received from server: " + data)
     udp.close()
@@ -25,4 +38,3 @@ def client():
 
 if __name__ == "__main__":
     client()
-
