7a653081eabc604ca668e7ee08be63efedfcb25b
rafaelsilvag <rafaelsg@gmail.com>
2021-12-16 14:01:38 -0500

    add app_cliente.py as an example

---
+++ aula07/app_cliente.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import requests
+from pprint import pprint
+
+url = "http://127.0.0.1:8080"
+
+cliente = {
+    "nome": "Joao Pedro",
+    "endereco": "Rua XYZ"
+}
+
+requests.post(f"{url}/cliente", json = cliente )
+
+r = requests.get(f"{url}/cliente")
+pprint(r.json())
